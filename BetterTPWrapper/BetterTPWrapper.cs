80f6c1bfe32ca7dddfa386ac794983c21f24a3ec
Zuppi <Zuppi@DESKTOP-2U2P03J>
2016-12-21 15:28:23 +0200

    Add project files.

---
+++ BetterTPWrapper/BetterTPWrapper.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterTPWrapper
+{
+    public partial class BetterTPWrapper : Form
+    {
+        public class LogItem
+        {
+            private string logLine;
+
+            public LogItem(string logLine)
+            {
+                this.logLine = logLine;
+            }
+
+            public override string ToString()
+            {
+                return logLine;
+            }
+
+        }
+        private bool allowVisible;     // ContextMenu's Show command used
+                                       //private bool allowClose;       // ContextMenu's Exit command used
+
+        System.Diagnostics.Process betterTP;
+
+        public BetterTPWrapper()
+        {
+            InitializeComponent();
+            allowVisible = false;
+            //allowClose = false;
+            betterTP = null;
+           
+            OpenBetterTP();
+        }
+
+        private void OpenBetterTP()
+        {
+            string betterTPPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "bettertp.exe");
+            if (!File.Exists(betterTPPath)){
+                AddLogLine("----- No BetterTP found. Please place the executable in same folder with bettertp.exe -----");
+            }
+            else
+            {
+                betterTP = new System.Diagnostics.Process();
+                betterTP.StartInfo.FileName = betterTPPath;
+                betterTP.StartInfo.CreateNoWindow = true;
+                betterTP.StartInfo.UseShellExecute = false;
+                betterTP.StartInfo.RedirectStandardOutput = true;
+                betterTP.StartInfo.RedirectStandardError = true;
+                betterTP.OutputDataReceived += OutputHandler;
+                betterTP.ErrorDataReceived += OutputHandler;
+                betterTP.Start();
+                betterTP.BeginErrorReadLine();
+                betterTP.BeginOutputReadLine();
+            }
+            
+           
+        }
+
+        void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            try
+            {
+                listBoxLog.Invoke((MethodInvoker)(() =>
+                {
+                    if (!String.IsNullOrEmpty(outLine.Data) && outLine.Data[0] != '[')
+                    {
+                        listBoxLog.Items.Add(new LogItem(outLine.Data));
+                    }
+
+                }));
+            }
+            
+            catch
+            {}
+        }
+
+        private void AddLogLine(string lineText)
+        {
+            listBoxLog.Items.Add(new LogItem(lineText));
+            listBoxLog.TopIndex = listBoxLog.Items.Count - 1;
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!allowVisible)
+            {
+                value = false;
+                if (!this.IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                allowVisible = true;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.BringToFront();
+            }
+        }
+
+        private void betterTPWrapper_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (betterTP != null)
+            {
+                if (!betterTP.HasExited)
+                {
+                    betterTP.Kill();
+                }
+            }         
+        }
+
+        private void BetterTPWrapper_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon.Visible = true;
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon.Visible = false;
+                this.BringToFront();
+            }
+        }
+    }
+}
---
+++ BetterTPWrapper/betterTPWrapper.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BetterTPWrapper
+{
+    partial class BetterTPWrapper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BetterTPWrapper));
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 12);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(585, 381);
+            this.listBoxLog.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(522, 404);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "BetterTP Wrapper";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // BetterTPWrapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 433);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.listBoxLog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BetterTPWrapper";
+            this.Text = "BetterTP Wrapper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.betterTPWrapper_FormClosing);
+            this.Resize += new System.EventHandler(this.BetterTPWrapper_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
